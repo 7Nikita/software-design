a87728d3be9e7406b6aaf69ce37aed322baa6a52
Nikita Pekurin <nikita.mda@yandex.by>
2019-11-07 13:14:30 +0300

    added landscape support

--- Calculator/app/src/main/java/com/nikita/calculator/activity/CalculatorActivity.java
+++ Calculator/app/src/main/java/com/nikita/calculator/activity/CalculatorActivity.java
@@ -1,10 +1,16 @@
 package com.nikita.calculator.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import com.nikita.calculator.ControlsAdapter;
@@ -19,6 +25,8 @@ import java.util.List;
 
 public class CalculatorActivity extends AppCompatActivity {
 
+    private EditText expression;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +38,23 @@ public class CalculatorActivity extends AppCompatActivity {
                 new ScientificControlsFragment()
         );
 
-        final ViewPager viewPager = findViewById(R.id.frame);
-        viewPager.setAdapter(new ControlsAdapter(getSupportFragmentManager(), fragmentList));
-        //viewPager.setCurrentItem(0);
+        expression = findViewById(R.id.expressionText);
+
+        int orientation = this.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            final ViewPager viewPager = findViewById(R.id.frame);
+            viewPager.setAdapter(new ControlsAdapter(getSupportFragmentManager(), fragmentList));
+        } else {
+            final FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction
+                    .add(R.id.basicMode, fragmentList.get(0))
+                    .add(R.id.scientificMode, fragmentList.get(1))
+                    .commit();
+        }
+    }
+
+    public void onKeyPress(@NonNull View view) {
+        expression.append(((Button) view).getText());
     }
 }
 
