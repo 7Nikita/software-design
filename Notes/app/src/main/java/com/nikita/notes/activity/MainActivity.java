69958bea4f1db41194f28f6fbe22be0f60e66f14
Nikita Pekurin <nikita.mda@yandex.by>
2019-11-24 18:32:19 +0300

    added dependencies

---
+++ Notes/app/src/main/java/com/nikita/notes/activity/MainActivity.java
@@ -0,0 +1,40 @@
+package com.nikita.notes;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter noteAdapter = new NoteAdapter();
+
+        recyclerView.setAdapter(noteAdapter);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                noteAdapter.setNotes(notes);
+            }
+        });
+
+    }
+
+}
